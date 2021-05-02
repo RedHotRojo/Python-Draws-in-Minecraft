061ae5c8b05aad6c9651beb5afa9ee8cb5f67adc
RedHotRojo <gayali.rohit@gmail.com>
2021-05-02 10:38:39 -0400

    Anonymize folders/files

--- avgIMG.py
+++ avgIMG.py
@@ -2,17 +2,17 @@
 Only tested in Windows
 Note: this file is unrequired; you can just download allBlocks.txt. This is just here to show you how I have created allBlocks.txt
 Basically, it gets the average color of **almost** every block.
-I got the textures from C:\Users\Rohit\AppData\Roaming\.minecraft\versions\21w16a\21w16a.jar (extracted the files to a folder)
+I got the textures from C:\Users\USER\AppData\Roaming\.minecraft\versions\21w16a\21w16a.jar (extracted the files to a folder)
 I deleted textures like glass, doors, plants, the texture for the sides of texture (like crafting_table_side)
 I renamed textures like crafting_table_top to crafting_table
 """
 from PIL import Image # pip install pillow
 import os # built-in
-files = os.listdir('C:\\Users\\Rohit\\Documents\\21w16a_textures') # This is where I stored the images that I kept
-f = open(f"C:\\Users\\Rohit\\Documents\\21w16a_textures\\allBlocks.txt", 'w') # The file where the names and colors will the stored.
+files = os.listdir('C:\\Users\\USER\\Documents\\textures') # This is where I stored the images that I kept
+f = open(f"C:\\Users\\USER\\Documents\\textures\\allBlocks.txt", 'w') # The file where the names and colors will the stored.
 for file in files: # Iterate through each png file
     if file.split('.')[1] == "png": # Make sure that non-png files are excluded
-        thisImg = Image.open(f"C:\\Users\\Rohit\\Documents\\21w16a_textures\\{file}").convert("RGBA") # Open the file and convert it to RGBA rather than the automatic RGBa (where alpha is ignored and PIL tries to calculate the color without the alpha; Their calculations are pretty bad :/ )
+        thisImg = Image.open(f"C:\\Users\\USER\\Documents\\textures\\{file}").convert("RGBA") # Open the file and convert it to RGBA rather than the automatic RGBa (where alpha is ignored and PIL tries to calculate the color without the alpha; Their calculations are pretty bad :/ )
         loaded = thisImg.load() # Load the image
         width, height = thisImg.size # Easy access
         clr = [0, 0, 0] # Initialize color
